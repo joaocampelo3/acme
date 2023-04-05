dbce800243d7ba51738cdd67a32cf4556715739c
João Campelo <1151386@isep.ipp.pt>
2023-04-05 12:19:38 +0100

    [Review] Save Products

--- Microservices/Reviews/src/main/java/com/isep/acme/services/ReviewEventSubscriber.java
+++ Microservices/Reviews/src/main/java/com/isep/acme/services/ReviewEventSubscriber.java
@@ -2,40 +2,27 @@ package com.isep.acme.services;
 
 import com.isep.acme.events.ProductEvent;
 import com.isep.acme.events.ReviewEvent;
-import com.isep.acme.model.DTO.CreateReviewDTO;
-import com.isep.acme.model.DTO.ReviewDTO;
 import com.isep.acme.model.Product;
-import com.isep.acme.model.Review;
 import com.isep.acme.repositories.ProductRepository;
-import com.isep.acme.services.impl.ReviewServiceImpl;
-import com.isep.acme.services.interfaces.ReviewService;
 import com.rabbitmq.client.*;
-import lombok.RequiredArgsConstructor;
-import net.bytebuddy.implementation.bind.annotation.Super;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import javax.inject.Inject;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
 @Component
+@EnableAsync
 public class ReviewEventSubscriber {
 
     private static final String EXCHANGE_NAME = "product_events";
 
     @Autowired
-    private final ProductRepository productRepository;
-
-    @Autowired
-    public ReviewEventSubscriber(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+    private ProductRepository productRepository;
 
 
     public void start() throws IOException, TimeoutException {
@@ -60,7 +47,7 @@ public class ReviewEventSubscriber {
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                String message = new String(body, "UTF-8");
+                String message = new String(body, StandardCharsets.UTF_8);
                 String eventType = envelope.getRoutingKey().substring(envelope.getRoutingKey().lastIndexOf(".") + 1);
                 String originService = envelope.getRoutingKey().substring(0, envelope.getRoutingKey().indexOf("."));
                 System.out.println("Received event '" + eventType + "' from service '" + originService + "' with message '" + message + "'");
@@ -84,7 +71,8 @@ public class ReviewEventSubscriber {
 
         // keep the subscriber running
         System.out.println("Waiting for events...");
-        while (true) {}
+        while (true) {
+        }
     }
 
     private void handleProductEvent(String eventType, String originService, ProductEvent event) throws Exception {
@@ -93,7 +81,6 @@ public class ReviewEventSubscriber {
             // do something with the product created event
             final Product p = new Product(event.getSku());
             productRepository.save(p);
-           // productRepository.save(p);
         } else if (eventType.equals("product_updated")) {
             // do something with the product updated event
         } else if (eventType.equals("product_deleted")) {
@@ -112,78 +99,10 @@ public class ReviewEventSubscriber {
         }
     }
 
-    public static void main(String[] args) throws IOException, TimeoutException {
-        ReviewEventSubscriber subscriber = new ReviewEventSubscriber(new ProductRepository() {
-            @Override
-            public Optional<Product> getProductBySku(String sku) {
-                return Optional.empty();
-            }
-
-            @Override
-            public void deleteBySku(String sku) {
-
-            }
-
-            public <S extends Product> S save(S entity) {
-                return entity;
-            }
-
-            @Override
-            public <S extends Product> Iterable<S> saveAll(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public Optional<Product> findById(Long aLong) {
-                return Optional.empty();
-            }
-
-            @Override
-            public boolean existsById(Long aLong) {
-                return false;
-            }
-
-            @Override
-            public Iterable<Product> findAll() {
-                return null;
-            }
-
-            @Override
-            public Iterable<Product> findAllById(Iterable<Long> longs) {
-                return null;
-            }
-
-            @Override
-            public long count() {
-                return 0;
-            }
-
-            @Override
-            public void deleteById(Long aLong) {
-
-            }
-
-            @Override
-            public void delete(Product entity) {
-
-            }
-
-            @Override
-            public void deleteAllById(Iterable<? extends Long> longs) {
-
-            }
-
-            @Override
-            public void deleteAll(Iterable<? extends Product> entities) {
-
-            }
-
-            @Override
-            public void deleteAll() {
-
-            }
-        });
-        subscriber.start();
+    @Bean
+    @Async
+    public void mainBean() throws IOException, TimeoutException {
+        this.start();
     }
 
 }
