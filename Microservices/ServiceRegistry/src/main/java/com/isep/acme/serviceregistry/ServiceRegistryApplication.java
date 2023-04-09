cebcbba489c023f947e86c0f50300f06f419d9d4
João Campelo <1151386@isep.ipp.pt>
2023-04-09 21:41:33 +0100

    Revert "Rename ServiceRegistryApplication path"
    This reverts commit 6ca79faafff66f7f029c426457b9c936b014f496.

--- Microservices/ServiceRegistry/src/main/java/com/isep/acme/serviceregistry/ServiceRegistryApplication.java
+++ Microservices/ServiceRegistry/src/main/java/com/isep/acme/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.isep.acme;
+package com.isep.acme.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Microservices/ServiceRegistry/src/test/java/com/isep/acme/serviceregistry/ServiceRegistryApplicationTests.java
+++ Microservices/ServiceRegistry/src/test/java/com/isep/acme/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.isep.acme;
+package com.isep.acme.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
